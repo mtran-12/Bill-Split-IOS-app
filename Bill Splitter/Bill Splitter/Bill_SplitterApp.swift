f28a68e17c9e9adde0d0c3dda51cdc446e29d9d5
Matt Tran <matthewltran@gmail.com>
2022-10-14 19:26:03 -0700

    initial commit

---
+++ Bill Splitter/Bill Splitter/Bill_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bill_SplitterApp.swift
+//  Bill Splitter
+//
+//  Created by Matthew Tran on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct Bill_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
